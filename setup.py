c8e13eccae7cd587e8ebfbbb2210d7e8decc81e1
unpluggedcoder <unpluggedcoder@outlook.com>
2018-04-17 18:10:05 +0800

    Add markdown support for setup.py, again.

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
 from asynclog import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
     name='asynclog',
     version=__version__,
     description='Asynchronous log for python logging.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     author='unpluggedcoder',
     author_email='unpluggedcoder@outlook.com',
     url='https://github.com/unpluggedcoder/asynclog',
-    packages=['asynclog'],
-    package_data={
-        'asynclog': ['README.md', ]
-    },
+    packages=find_packages(),
+    package_data={'asynclog': [
+        'README.md',
+    ]},
     keywords='logging asynchronous',
     install_requires=[],
     classifiers=[
@@ -24,5 +35,4 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Logging',
-    ],
-)
+    ], )
